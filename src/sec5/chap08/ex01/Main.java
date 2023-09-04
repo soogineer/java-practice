718144b14cd037559444d85f59768cac9ee5ed47
soogineer <ysooroh54@gmail.com>
2023-09-04 09:13:16 +0900

    prac Abstract Class

---
+++ src/sec5/chap08/ex01/Main.java
@@ -0,0 +1,33 @@
+package sec5.chap08.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        //  ⚠️ 불가
+        YalcoGroup yalcoGroup = new YalcoGroup(1, "서울") {
+            @Override
+            public void takeOrder() {
+
+            }
+        };
+
+        YalcoChicken ychStore1 = new YalcoChicken("판교");
+        YalcoChicken ychStore2 = new YalcoChicken("강남");
+
+        YalcoCafe ycfStore1 = new YalcoCafe("울릉", true);
+        YalcoCafe ycfStore2 = new YalcoCafe("강릉", false);
+
+        YalcoGroup[] ycStores = {
+                ychStore1, ychStore2,
+                ycfStore1, ycfStore2
+        };
+
+        for (YalcoGroup ycStore : ycStores) {
+            ycStore.takeOrder();
+        }
+
+        System.out.println("\n- - - - -\n");
+
+        System.out.println(YalcoChicken.getCreed());
+        System.out.println(YalcoCafe.getCreed());
+    }
+}
